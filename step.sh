17a76253a5f91aea6882b0c4c31fa22c5e683caa
Ganesh Bala Subramanian <ganesh.ad.de@gmail.com>
2023-05-12 10:52:54 +0200

    Add step script and config

---
+++ step.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+set -ex
+
+echo "${devops_pr_state} ${devops_description}"
+
+state=$devops_pr_state
+description=$devops_description
+
+if [[ "$state" == "auto" ]]
+then
+    state="failed"
+
+    if [[ $BITRISE_BUILD_STATUS -eq 0 ]]
+    then
+        state="succeeded"
+    fi
+fi
+
+if [[ -z "$description" ]]
+then
+    description="${BITRISE_APP_TITLE} build #${BITRISE_BUILD_NUMBER} ${state}"
+fi
+
+URL="https://${devops_user}:${devops_pat}@dev.azure.com/${devops_organization}/${devops_project}/_apis/git/repositories/${devops_repository_id}/pullRequests/${BITRISE_PULL_REQUEST}/statuses?api-version=7.0"
+HTTP_RESPONSE=$(curl $URL --silent --write-out "HTTPSTATUS:%{http_code}" \
+-H "Content-Type: application/json" \
+-d @- <<EOF
+{
+    "state": "${state}",
+    "description": "${description}",
+    "targetUrl": "${BITRISE_BUILD_URL}",
+    "context":  {
+        "genre": "${devops_context_genre}",
+        "name": "${devops_context_name}"
+    }
+}
+EOF
+)
+
+HTTP_STATUS=$(echo $HTTP_RESPONSE | tr -d '\n' | sed -E 's/.*HTTPSTATUS:([0-9]{3})$/\1/')
+
+if [[ ! $HTTP_STATUS -eq 201 ]] && [[ ! $HTTP_STATUS -eq 200 ]]
+then
+  echo "Error [HTTP status: $HTTP_STATUS]"
+  exit 1
+fi
